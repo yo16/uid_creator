1343ea0943f5e3f8237263665bb685a8a989c373
yo16 <xxyo1xx@gmail.com>
2024-11-11 18:45:21 +0900

    add sample that uses nanoid

--- app-uid-creator/src/App.tsx
+++ app-uid-creator/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
 
+import { Nano } from './Nano';
+
 function App() {
-  return (
-    <>
-      app
-    </>
-  )
+
+
+    return (
+        <>
+            記号もあり<br />
+            {Array.from({ length: 10 }, (_, i) => (
+                <Nano key={i} length={8} />
+            ))}
+
+            <hr />
+            記号はなし<br />
+            {Array.from({ length: 10 }, (_, i) => (
+                <Nano key={i} length={8} usingSymboles={false} />
+            ))}
+        </>
+    );
 }
 
 export default App
---
+++ app-uid-creator/src/Nano.tsx
@@ -0,0 +1,37 @@
+import { nanoid, customAlphabet } from "nanoid"
+
+interface NanoProps {
+    length: number;
+    usingSymboles?: boolean;
+}
+export function Nano({
+    length,
+    usingSymboles = true,
+}: NanoProps) {
+
+    let id: string = "";
+    // Symbol（記号）を含めてはいけない場合は、カスタムアルファベットを使う
+    if (!usingSymboles) {
+        const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const customNanoid = customAlphabet(alphabet, length);
+        id = customNanoid();
+    } else {
+        id = nanoid(length);
+    }
+
+    return (
+        <>
+            <span
+                style={{
+                    padding: "5px",
+                    borderRadius: "3px",
+                    backgroundColor: "#ccc",
+                    color: "#333",
+                    margin: "2px",
+                }}
+            >
+                {id}
+            </span>
+        </>
+    );
+}
